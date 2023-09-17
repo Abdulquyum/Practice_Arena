9be17d30e39612e0276e9b5bc4bf7f2b0edeb1d7
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-17 11:03:41 +0100

    prints the reverse order of an initialized array

---
+++ reverse_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[] = {34, 56, 54, 32, 67, 89, 90, 32, 21};
+	int q, i;
+
+	for(q = 0; q < 9; q++)
+	{
+		printf("These are the numbers in my_array arr[%d]: %d\n", q, arr[q]);
+	}
+
+	printf("\n-----------------------------------\n\n");
+
+	for (i = 8; i >= 0; i--)
+	{
+		printf("Reverse order of my_array arr[%d]: %d\n", i, arr[i]);
+	}
+
+	return (0);
+}
